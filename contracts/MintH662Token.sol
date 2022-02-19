7e900b4117b7f76863779112e7feae798c117091
h662 <h662hong@gmail.com>
2022-02-19 17:01:02 +0900

    first commit

--- contracts/MintH662Token.sol
+++ contracts/MintH662Token.sol
@@ -5,18 +5,18 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MintH662Token is ERC721Enumerable, Ownable {
+contract MintLsbtdToken is ERC721Enumerable, Ownable {
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
 
     mapping(uint => string) tokenURIs;
 
     function tokenURI(uint _tokenId) override public view returns (string memory) {
         return string(
-            abi.encodePacked("https://h662.mypinata.cloud/ipfs/", tokenURIs[_tokenId])
+            abi.encodePacked("https://lsbtd.mypinata.cloud/ipfs/", tokenURIs[_tokenId])
         );
     }
 
-    function mintToken(string memory _tokenURI) public onlyOwner {
+    function mintLsbtdToken(string memory _tokenURI) public onlyOwner {
         uint tokenId = totalSupply() + 1;
 
         tokenURIs[tokenId] = _tokenURI;
